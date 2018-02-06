6c4c71d24acf4e5238569400dfea068962a18f00
Alisher Karibzhanov <alisktl@gmail.com>
2018-02-06 20:55:01 +0300

    Initial commit

---
+++ app/src/main/java/com/ru/alisher/earthquake/earthquake/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ru.alisher.earthquake.earthquake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
